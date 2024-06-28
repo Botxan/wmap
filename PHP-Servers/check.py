42106267102dd398e6f045e91d24ff8e8a6e2704
Botxan <oihan.irca@gmail.com>
2024-06-28 11:33:18 +0200

    Docker compose dynamic port selection. Closes #1

--- PHP-Servers/check.py
+++ PHP-Servers/check.py
@@ -5,28 +5,38 @@ import subprocess
 import argparse
 import re
 
-# Configuration
-base_url = "http://127.0.0.1:8080/wmap"
+def get_host_port():
+    result = subprocess.getoutput("docker-compose port web 80")
+    match = re.search(r':(\d+)$', result)
+    if match:
+        return match.group(1)
+    else:
+        print("Could not determine host port.")
+        sys.exit(1)
+
+host_port = get_host_port()
+base_url = f"http://127.0.0.1:{host_port}/wmap"
+
 frameworks_list = {
-    "cakephp-4.5": "http://127.0.0.1:8080/wmap/cakephp-4.5/webroot/index.php/hello/index",
-    "cakephp-5.0": "http://127.0.0.1:8080/wmap/cakephp-5.0/webroot/index.php/hello/index",
-    "codeigniter-4.4": "http://127.0.0.1:8080/wmap/codeigniter-4.4/public/index.php/hello/index",
-    "fastroute-1.3": "http://127.0.0.1:8080/wmap/fastroute-1.3/public/index.php/hello/index",
-    "fatfree-3.8": "http://127.0.0.1:8080/wmap/fatfree-3.8/public/index.php/hello/index",
-    "fuelphp-1.9": "http://127.0.0.1:8080/wmap/fuelphp-1.9/public/index.php/helloworld/index",
-    "kumbia-1.1": "http://127.0.0.1:8080/wmap/kumbia-1.1/default/public/index.php/helloworld/index",
-    "laravel-10.2": "http://127.0.0.1:8080/wmap/laravel-10.2/public/index.php/hello/index",
-    "leaf-3.5": "http://127.0.0.1:8080/wmap/leaf-3.5/public/index.php/hello/index",
-    "lumen-10.0": "http://127.0.0.1:8080/wmap/lumen-10.0/public/index.php/hello/index",
-    "phroute-2.2": "http://127.0.0.1:8080/wmap/phroute-2.2/public/index.php/hello/index",
-    "pure-php": "http://127.0.0.1:8080/wmap/pure-php/public/index.php/hello/index",
-    "silex-2.3": "http://127.0.0.1:8080/wmap/silex-2.3/web/index.php/hello/index",
-    "slim-4.12": "http://127.0.0.1:8080/wmap/slim-4.12/public/index.php/hello/index",
-    "symfony-5.4": "http://127.0.0.1:8080/wmap/symfony-5.4/public/index.php/hello/index",
-    "symfony-6.4": "http://127.0.0.1:8080/wmap/symfony-6.4/public/index.php/hello/index",
-    "symfony-7.0": "http://127.0.0.1:8080/wmap/symfony-7.0/public/index.php/hello/index",
-    "ubiquity-2.4.x.dev": "http://127.0.0.1:8080/wmap/ubiquity-2.4.x.dev/public/index.php?c=HelloWorldController/index",
-    "yii-2.0-basic": "http://127.0.0.1:8080/wmap/yii-2.0-basic/web/index.php?r=helloworld/index"
+    "cakephp-4.5": base_url + "/cakephp-4.5/webroot/index.php/hello/index",
+    "cakephp-5.0": base_url + "/cakephp-5.0/webroot/index.php/hello/index",
+    "codeigniter-4.4": base_url + "/codeigniter-4.4/public/index.php/hello/index",
+    "fastroute-1.3": base_url + "/fastroute-1.3/public/index.php/hello/index",
+    "fatfree-3.8": base_url + "/fatfree-3.8/public/index.php/hello/index",
+    "fuelphp-1.9": base_url + "/fuelphp-1.9/public/index.php/helloworld/index",
+    "kumbia-1.1": base_url + "/kumbia-1.1/default/public/index.php/helloworld/index",
+    "laravel-10.2": base_url + "/laravel-10.2/public/index.php/hello/index",
+    "leaf-3.5": base_url + "/leaf-3.5/public/index.php/hello/index",
+    "lumen-10.0": base_url + "/lumen-10.0/public/index.php/hello/index",
+    "phroute-2.2": base_url + "/phroute-2.2/public/index.php/hello/index",
+    "pure-php": base_url + "/pure-php/public/index.php/hello/index",
+    "silex-2.3": base_url + "/silex-2.3/web/index.php/hello/index",
+    "slim-4.12": base_url + "/slim-4.12/public/index.php/hello/index",
+    "symfony-5.4": base_url + "/symfony-5.4/public/index.php/hello/index",
+    "symfony-6.4": base_url + "/symfony-6.4/public/index.php/hello/index",
+    "symfony-7.0": base_url + "/symfony-7.0/public/index.php/hello/index",
+    "ubiquity-2.4.x.dev": base_url + "/ubiquity-2.4.x.dev/public/index.php?c=HelloWorldController/index",
+    "yii-2.0-basic": base_url + "/yii-2.0-basic/web/index.php?r=helloworld/index"
 }
 
 GREEN = '\033[0;32m'
@@ -50,7 +60,7 @@ Usage: python3 check.py [-t pure-php slim-*]
 
 Optional Arguments:
     -h, --help                  Show this help message and exit
-    -t, --target                Specify your target framework/s for benchmarking
+    -t, --target                Specify your target framework/s
                                 Separate them by spaces.
 '''
     print(help_text)
@@ -58,8 +68,8 @@ Optional Arguments:
 
 def main():
 
-    parser = argparse.ArgumentParser(description="Benchmark Frameworks", add_help=False)
-    parser.add_argument("-t", "--target", nargs='*', help="Specify your target framework/s for benchmarking")
+    parser = argparse.ArgumentParser(description="Frameworks", add_help=False)
+    parser.add_argument("-t", "--target", nargs='*', help="Specify your target framework/s")
     parser.add_argument("-h", "--help", action='store_true', help="Show this help message and exit")
 
     args = parser.parse_args()

8efc10826820119d3bcc1047ace703061e58d592
Botxan <oihan.irca@gmail.com>
2024-08-10 20:33:52 +0200

    Updated logger with macros to allow variable number of args

--- wmap/src/logger.rs
+++ wmap/src/logger.rs
@@ -1,71 +1,104 @@
 use clap::ArgMatches;
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
 
 pub struct Logger {
     verbose: bool,
+    output_file: Option<File>,
 }
 
 impl Logger {
-    pub fn new(verbose: bool) -> Logger {
-        Logger { verbose }
+    pub fn new(verbose: bool, output: Option<&str>) -> Logger {
+        let output_file = output.map(|path| File::create(Path::new(path)).expect("Failed to create output file"));
+        Logger { verbose, output_file }
     }
 
-    pub fn print(&self, message: &str) {
-        println!("{}", message);
+    pub fn print(&mut self, message: &str) {
+        self.print_to_target(format_args!("{}", message));
     }
 
-    pub fn print_verbose(&self, message: &str) {
+    pub fn print_verbose(&mut self, message: &str) {
         if self.verbose {
-            println!("{}", message);
+            self.print(message);
         }
     }
 
-    pub fn print_args(&self, matches: &ArgMatches) {
+    pub fn print_args(&mut self, matches: &ArgMatches) {
         if self.verbose {
             self.print_formatted_args(matches);
         }
     }
 
-    fn print_formatted_args(&self, matches: &clap::ArgMatches) {
-        // URL
-        if let Some(url) = matches.get_one::<String>("url") {
-            println!("URL: {}", url);
+    pub fn print_format(&mut self, format: std::fmt::Arguments) {
+        self.print_to_target(format);
+    }
+
+    fn print_to_target(&mut self, format: std::fmt::Arguments) {
+        if let Some(ref mut file) = self.output_file {
+            writeln!(file, "{}", format).expect("Failed to write to output file");
         } else {
-            println!("No URL provided");
+            println!("{}", format);
+        }
+    }
+
+    pub fn is_verbose(&self) -> bool {
+        self.verbose
+    }
+
+    fn print_formatted_args(&mut self, matches: &clap::ArgMatches) {
+        self.print("Command line arguments:");
+
+        if let Some(url) = matches.get_one::<String>("url") {
+            self.print_format(format_args!("> URL: {}", url));
+        } else {
+            self.print_format(format_args!("[x] No URL provided"));
         }
 
-        // Methods
         if let Some(methods) = matches.get_many::<String>("methods") {
-            println!("HTTP Methods:");
+            self.print_format(format_args!("> HTTP Methods:"));
             for method in methods {
-                println!("  - {}", method);
+                self.print_format(format_args!("  - {}", method));
             }
         }
 
-        // Encoding
         if let Some(encoding) = matches.get_one::<String>("encoding") {
-            println!("Encoding: {}", encoding);
+            self.print_format(format_args!("> Encoding: {}", encoding));
         }
 
-        // Output file
         if let Some(output) = matches.get_one::<String>("output") {
-            // Convert to absolute path if possible
             match std::fs::canonicalize(output) {
-                Ok(abs_path) => println!(
-                    "Output file: {} (absolute path: {})",
+                Ok(abs_path) => self.print_format(format_args!(
+                    "> Output file: {} (absolute path: {})",
                     output,
                     abs_path.display()
-                ),
-                Err(_) => println!("Output file: {}", output), // If it fails, print it as introduced by the user
+                )),
+                Err(_) => self.print_format(format_args!("> Output file: {}", output)),
             }
         } else {
-            println!("No output file specified.");
+            self.print_format(format_args!("[x] No output file specified."));
         }
 
-        // Verbose
         if matches.get_flag("verbose") {
-            println!("Verbose mode enabled.");
+            self.print_format(format_args!("> Verbose mode enabled."));
         } else {
-            println!("Verbose mode disabled.");
+            self.print_format(format_args!("> Verbose mode disabled."));
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! log_print {
+    ($logger:expr, $($arg:tt)*) => {
+        $logger.print_format(format_args!($($arg)*));
+    }
+}
+
+#[macro_export]
+macro_rules! log_print_verbose {
+    ($logger:expr, $($arg:tt)*) => {
+        if $logger.is_verbose() {
+            $logger.print_format(format_args!($($arg)*));
         }
     }
 }

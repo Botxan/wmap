52f0d2bb62b1e77a36917368080c96b78e72a5ed
Botxan <oihan.irca@gmail.com>
2024-08-10 20:30:32 +0200

    Changed line width in rustfmt

--- wmap/src/args.rs
+++ wmap/src/args.rs
@@ -3,23 +3,15 @@ use clap::{command, Arg, ArgAction};
 pub fn parse_args() -> clap::ArgMatches {
     command!()
         .about("HTTP request fuzzing CLI tool for web framework detection")
-        .arg(
-            Arg::new("url")
-                .short('u')
-                .long("url")
-                .help("Target URL")
-                .required(true),
-        )
+        .arg(Arg::new("url").short('u').long("url").help("Target URL").required(true))
         .arg(
             Arg::new("methods")
                 .short('m')
                 .long("methods")
                 .help("List of HTTP methods to mutate")
                 .value_delimiter(',')
-                .value_parser([
-                    "GET", "HEAD", "POST", "PUT", "DELETE", "CONNECT", "OPTIONS", "TRACE", "PATCH",
-                ])
-                .default_values(["GET", "HEAD"]),
+                .value_parser(["GET", "HEAD", "POST", "PUT", "DELETE", "CONNECT", "OPTIONS", "TRACE", "PATCH"])
+                .default_values(["GET"]),
         )
         .arg(
             Arg::new("encoding")
@@ -29,12 +21,7 @@ pub fn parse_args() -> clap::ArgMatches {
                 .value_parser(["ASCII", "UTF-8"])
                 .default_value("ASCII"),
         )
-        .arg(
-            Arg::new("output")
-                .short('o')
-                .long("output")
-                .help("Output file"),
-        )
+        .arg(Arg::new("output").short('o').long("output").help("Output file"))
         .arg(
             Arg::new("verbose")
                 .short('v')

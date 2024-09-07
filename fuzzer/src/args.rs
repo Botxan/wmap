d72c279b46a8db4a34746dc96a56b57a9c86a901
Botxan <oihan.irca@gmail.com>
2024-09-07 11:30:31 +0200

    Grouped --url and --input args for exclusive selection

--- fuzzer/src/args.rs
+++ fuzzer/src/args.rs
@@ -1,18 +1,10 @@
-use clap::{command, Arg, ArgAction};
+use clap::{command, Arg, ArgAction, ArgGroup};
 
 pub fn parse_args() -> clap::ArgMatches {
     command!()
         .about("HTTP request fuzzing CLI tool for web framework detection")
-        .arg(Arg::new("url").short('u').long("url").value_name("URL or URL;FRAMEWORK").help("Target URL").required(true))
-        .arg(
-            Arg::new("input")
-                .short('i')
-                .long("input")
-                .value_name("FILE")
-                .help("Input file with list of URLs")
-                .conflicts_with("url")
-                .required(true),
-        )
+        .arg(Arg::new("url").short('u').long("url").value_name("URL or URL;FRAMEWORK").help("Target URL"))
+        .arg(Arg::new("input").short('i').long("input").value_name("FILE").help("Input file with list of URLs"))
         .arg(
             Arg::new("methods")
                 .short('m')
@@ -21,7 +13,7 @@ pub fn parse_args() -> clap::ArgMatches {
                 .help("List of HTTP methods to mutate")
                 .value_delimiter(',')
                 .value_parser(["GET", "HEAD", "POST", "PUT", "DELETE", "CONNECT", "OPTIONS", "TRACE", "PATCH"])
-                .default_values(["GET"]),
+                .default_values(["GET", "HEAD"]),
         )
         .arg(Arg::new("output").short('o').long("output").value_name("FILE").help("Write output to a file"))
         .arg(
@@ -29,10 +21,11 @@ pub fn parse_args() -> clap::ArgMatches {
                 .long("output-format")
                 .short('f')
                 .value_name("FORMAT")
-                .help("Specifiy the output format")
+                .help("Specify the output format")
                 .value_parser(["json", "csv"])
                 .default_value("json"),
         )
         .arg(Arg::new("verbose").short('v').long("verbose").help("Display additional information").action(ArgAction::SetTrue))
+        .group(ArgGroup::new("required_group").args(&["url", "input"]).required(true))
         .get_matches()
 }
